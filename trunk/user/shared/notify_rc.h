93179896d4725734bd4e9fee757edfd6f3113c48
h70076333 <h70076222@88.com>
2025-01-18 19:39:01 +0800

    Update notify_rc.h

--- trunk/user/shared/notify_rc.h
+++ trunk/user/shared/notify_rc.h
@@ -63,12 +63,14 @@
 #define RCN_RESTART_SCUT		"restart_scutclient"
 #define RCN_RESTART_MENTOHUST	"restart_mentohust"
 #define RCN_RESTART_TTYD		"restart_ttyd"
+#define RCN_RESTART_ZEROTIER		"restart_zerotier"
 #define RCN_RESTART_VLMCSD		"restart_vlmcsd"
 #define RCN_RESTART_SHADOWSOCKS	"restart_shadowsocks"
 #define RCN_RESTART_CHNROUTE_UPD	"restart_chnroute_upd"
 #define RCN_RESTART_DNSFORWARDER	"restart_dns_forwarder"
 #define RCN_RESTART_SS_TUNNEL		"restart_ss_tunnel"
 #define RCN_RESTART_GFWLIST_UPD		"restart_gfwlist_upd"
+#define RCN_RESTART_WIREGUARD	"restart_wireguard"
 #define RCN_RESTART_REBOOT		"restart_reboot"
 
 ////////////////////////////////////////////////////////////
